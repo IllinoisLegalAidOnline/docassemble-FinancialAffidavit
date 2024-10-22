9368b60baf3213acdc81b0ff3410c85d3f689d16
Jim Graszer <jim@lemmalegal.com>
2024-10-22 16:08:40 -0500

    Fixes from review, updated employment section and additional employment/business PDF, added income taxes

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.FinancialAffidavit',
-      version='0.0.1',
+      version='0.0.3',
       description=('A docassemble extension.'),
       long_description='# docassemble.FinancialAffidavit\n\nA docassemble extension.\n\n## Author\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
       long_description_content_type='text/markdown',
